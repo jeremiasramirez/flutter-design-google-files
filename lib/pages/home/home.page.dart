0493d165ba3c92cfc90507f373160a23e808d2d8
Jeremias ramirez <jereramirez499@gmail.com>
2021-04-11 10:00:19 -0400

    agregado separador de widget

--- lib/pages/home/home.page.dart
+++ lib/pages/home/home.page.dart
@@ -1,11 +1,23 @@
 import "package:flutter/material.dart";
+import 'package:folders_file/widgets/separated/separated.widget.dart';
 
 class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(title: Text("app bar")),
-        ));
+        home: Scaffold(body: ListView(children: [AppbarNavigate()])));
+  }
+}
+
+class AppbarNavigate extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text("View your"),
+        Row(children: [Icon(Icons.settings), Icon(Icons.more_vert_outlined)])
+      ],
+    ));
   }
 }
