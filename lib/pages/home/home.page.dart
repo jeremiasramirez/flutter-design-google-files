a6dfcef9827323d6162d23e5a519aa9b4cffc49f
Jeremias ramirez <jereramirez499@gmail.com>
2021-04-11 23:36:56 -0400

    agregando search widget

--- lib/pages/home/home.page.dart
+++ lib/pages/home/home.page.dart
@@ -1,10 +1,45 @@
 import "package:flutter/material.dart";
 import 'package:folders_file/widgets/appbar/appbar.navigate.dart';
+import 'package:folders_file/widgets/separated/separated.widget.dart';
 
 class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(body: ListView(children: [AppbarNavigate()])));
+        home: Scaffold(
+            body: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.blue[100], Colors.white])),
+                child: ListView(children: [
+                  AppbarNavigate(),
+                  this.title(),
+                  Separated(0, 25.0),
+                  this.search()
+                ]))));
+  }
+
+  Container title() {
+    return Container(
+        padding: EdgeInsets.only(top: 25.0, right: 10, left: 10),
+        child: Text("Recents",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 28,
+                color: Colors.blue[900])));
+  }
+
+  Container search() {
+    return Container(
+      margin: EdgeInsets.all(7.0),
+      child: TextField(
+        cursorColor: Colors.black,
+        decoration: InputDecoration(
+            fillColor: Colors.white,
+            icon: Icon(Icons.search),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+            hintText: "Search"),
+      ),
+    );
   }
 }
--- lib/widgets/appbar/appbar.navigate.dart
+++ lib/widgets/appbar/appbar.navigate.dart
@@ -1,14 +1,27 @@
 import "package:flutter/material.dart";
 
 class AppbarNavigate extends StatelessWidget {
-  Widget build(context) {
+  Container build(context) {
     return Container(
+        padding: EdgeInsets.only(top: 25.0, right: 10, left: 10),
         child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text("View your"),
-        Row(children: [Icon(Icons.settings), Icon(Icons.more_vert_outlined)])
-      ],
-    ));
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            this.title(),
+            Row(children: [
+              Icon(Icons.settings, color: Colors.grey[700]),
+              Icon(Icons.more_vert_outlined, color: Colors.grey[700])
+            ])
+          ],
+        ));
+  }
+
+  Text title() {
+    return Text(" View your",
+        style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[800],
+            fontFamily: "arial"));
   }
 }
